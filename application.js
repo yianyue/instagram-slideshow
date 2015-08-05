43531b4cf083d747a0175701c482ede60ba46a7f
yianyue <yianyue@gmail.com>
2015-08-05 17:22:38 -0400

    got response back

--- application.js
+++ application.js
@@ -1,7 +1,15 @@
-// my access token: 1422443644.1fb234f.da4d709164014af592cb05946caa1125
+// access token: 1422443644.1fb234f.da4d709164014af592cb05946caa1125
+// client id: b49c63480edd40ee96c3bf68cfeef191
+var TOKEN = '1422443644.1fb234f.da4d709164014af592cb05946caa1125';
+var tag = 'lighthouse';
 
 $(document).ready(function(){
-
-  console.log('hello');
+ $.ajax({
+        method: "GET",
+        url: 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token='+TOKEN,
+        dataType: 'jsonp'
+      }).done(function(response){
+        console.log(response);
+        });
 
 });
