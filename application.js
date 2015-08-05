a2252157fb042bd00dac372afbe3fded1c1294e5
yianyue <yianyue@gmail.com>
2015-08-05 17:44:06 -0400

    extracted image url from response data

--- application.js
+++ application.js
@@ -1,15 +1,28 @@
 // access token: 1422443644.1fb234f.da4d709164014af592cb05946caa1125
 // client id: b49c63480edd40ee96c3bf68cfeef191
-var TOKEN = '1422443644.1fb234f.da4d709164014af592cb05946caa1125';
-var tag = 'lighthouse';
 
 $(document).ready(function(){
- $.ajax({
-        method: "GET",
-        url: 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token='+TOKEN,
-        dataType: 'jsonp'
-      }).done(function(response){
-        console.log(response);
-        });
+  var TOKEN = '1422443644.1fb234f.da4d709164014af592cb05946caa1125';
+  var tag = 'lighthouse';
+
+  // DOM manipulation
+  var displayImage= function(imgUrl){
+    var frame = $('#main-image');
+    frame.empty;
+    frame.append('<img src="'+imgUrl+'">');
+  }
+
+  $.ajax({
+    method: "GET",
+    url: 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token='+TOKEN,
+    dataType: 'jsonp'
+  }).done(function(response){
+    // var nextUrl = response.pagination.next_url;
+    var images = response.data.map(function(obj){
+      return obj.images.standard_resolution.url;
+    });
+    displayImage(images[1]);
+  });
+
 
 });
