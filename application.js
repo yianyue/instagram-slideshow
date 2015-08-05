982188ccf2ac1ac4df4f2909ec9b9e5f1590a043
yianyue <yianyue@gmail.com>
2015-08-05 18:51:17 -0400

    show images at 2sec interval

--- application.js
+++ application.js
@@ -2,27 +2,30 @@
 // client id: b49c63480edd40ee96c3bf68cfeef191
 
 $(document).ready(function(){
-  var TOKEN = '1422443644.1fb234f.da4d709164014af592cb05946caa1125';
+  var clientId = 'b49c63480edd40ee96c3bf68cfeef191';
   var tag = 'lighthouse';
 
   // DOM manipulation
-  var displayImage= function(imgUrl){
-    var frame = $('#main-image');
-    frame.empty;
-    frame.append('<img src="'+imgUrl+'">');
+  var slideShow = function(urls){    
+    var imgTag = $('#main-image img');  
+    var i = 0;
+    var interval = setInterval(function(){
+      imgTag.attr('src', urls[i]);
+      i += 1;
+      if(i == 10) clearInterval(interval);
+    }, 2000);
   }
 
   $.ajax({
     method: "GET",
-    url: 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token='+TOKEN,
+    url: 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?client_id='+clientId,
     dataType: 'jsonp'
   }).done(function(response){
     // var nextUrl = response.pagination.next_url;
     var images = response.data.map(function(obj){
       return obj.images.standard_resolution.url;
     });
-    displayImage(images[1]);
+    slideShow(images);
   });
-
 
 });
